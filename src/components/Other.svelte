505ef87664bec4df1a810d0ced565fa989e78ad1
Zolo101 <umadeguy@gmail.com>
2024-03-24 03:19:40 +0000

    Correct link components

--- src/components/Other.svelte
+++ src/components/Other.svelte
@@ -4,5 +4,5 @@
 
 <section class="w-full ring-1 rounded text-2xl p-1 text-center no-underline cursor-pointer transition-colors">
 <!--    <div class="bg-green-100 blur"></div>-->
-    <a {href}><slot></slot></a>
+    <a {href} class="w-full h-full block"><slot></slot></a>
 </section>
--- src/components/Social.svelte
+++ src/components/Social.svelte
@@ -5,6 +5,5 @@
 </script>
 
 <section class="flex flex-col gap-1 bg-purple-950/15 hover:bg-purple-950/40">
-    <img width="64" height="64" src={image}/>
-    <a {href} class="text-xs text-center"><slot></slot></a>
+    <a {href} class="text-xs text-center"><img width="64" height="64" src={image}/><slot></slot></a>
 </section>
