70a68d7ad9378a9b2798c60ba0868c22f00d6796
Zelo101 <umadeguy@gmail.com>
2025-11-28 01:15:10 +0000

    +error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { page } from "$app/state";
+</script>
+
+<div class="flex items-end gap-8 italic">
+    <h1 class="text-8xl font-black">{page.status}</h1>
+    <p class="text-3xl">{page.error?.message}</p>
+</div>
+
+<style>
+</style>
